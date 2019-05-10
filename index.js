bb4f6ff3b8c7277142d02628c551353245bbe12c
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-05-10 15:59:49 -0400

    self invoking async function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Toolkit } = require('actions-toolkit');
 
-const tools = new Toolkit({
-  event: ['pull_request'],
-});
+(async function() {
+  const tools = new Toolkit({
+    event: ['pull_request'],
+  });
 
-const result = await tools.runInWorkspace('npm', ['audit']);
+  const result = await tools.runInWorkspace('npm', ['audit']);
 
-await tools.github.issues.create({
-  ...tools.context.issue,
-  body: `## npm audit report\n\n\`\`\`\n${result}\n\`\`\``,
-});
+  await tools.github.issues.create({
+    ...tools.context.issue,
+    body: `## npm audit report\n\n\`\`\`\n${result}\n\`\`\``,
+  });
+})();
